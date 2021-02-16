db2ea77dfaa7c463eaec005666e48f702ee87758
Matt Condon (shrugs) <mattgcondon@gmail.com>
2021-02-16 10:01:05 -0500

    chore: move contracts to themanymatts project

--- server/lib/chain.ts
+++ server/lib/chain.ts
@@ -1,9 +1,7 @@
+import ERC1155PresetMinterPauserArtifact from 'common/lib/contracts/ERC1155PresetMinterPauser.json';
 import { Granter } from 'common/lib/granter';
 import { DropGrant, GrantType } from 'common/lib/schemas/DropGrantSchema';
 import { ethers } from 'ethers';
-
-import ERC1155PresetMinterPauserArtifact from '../../../contracts/artifacts/@openzeppelin/contracts/presets/ERC1155PresetMinterPauser.sol/ERC1155PresetMinterPauser.json';
-import { ERC1155PresetMinterPauser } from '../../../contracts/typechain/ERC1155PresetMinterPauser';
 
 const provider =
   process.env.NODE_ENV === 'development'
@@ -14,11 +12,7 @@ const signer = new ethers.Wallet(process.env.SIGNER_PRIVATE_KEY, provider);
 const EMPTY_DATA = ethers.utils.arrayify(0);
 
 function getContract(address: string) {
-  return (new ethers.Contract(
-    address,
-    ERC1155PresetMinterPauserArtifact.abi,
-    signer,
-  ) as unknown) as ERC1155PresetMinterPauser;
+  return new ethers.Contract(address, ERC1155PresetMinterPauserArtifact.abi, signer);
 }
 
 // verifies that a grant is valid on-chain and will most likely succeed
